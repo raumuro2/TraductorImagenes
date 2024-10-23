ae2b1f7e78e085a14c5f93fbe54abc7aca4a4360
raumuro2 <raumuro2@gmail.com>
2024-10-23 14:08:43 +0200

    actualizacion traduccionWindow

--- TraductorImagenes/TraduccionWindow.xaml.cs
+++ TraductorImagenes/TraduccionWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace TraductorImagenes
             InitializeComponent();
             txt_Response.Text = response;
         }
+
+        private void btn_atras_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                Point mousePos = e.GetPosition(this);
+
+                this.DragMove();
+
+                // Verifica que la ventana no salga de la pantalla
+                double screenWidth = SystemParameters.PrimaryScreenWidth;
+                double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+                if (this.Left < 0) this.Left = 0;
+                if (this.Top < 0) this.Top = 0;
+                if (this.Left + this.Width > screenWidth) this.Left = screenWidth - this.Width;
+                if (this.Top + this.Height > screenHeight) this.Top = screenHeight - this.Height;
+            }
+        }
+
+        private void btn_cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
